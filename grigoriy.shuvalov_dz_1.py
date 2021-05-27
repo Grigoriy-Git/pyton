148a88a252f4deacaaef974ff7855adc3f8b0843
Grigoriy Shuvalov <grigoriy.shuvalov@mail.ru>
2021-05-27 12:17:37 +0300

    lesson-1

--- grigoriy.shuvalov_dz_1.py
+++
@@ -1,6 +0,0 @@
-duration = input('Введите число: ')
-if duration.isdigit():
-    duration = int(duration)
-    print(duration // 3600, duration // 60 % 60, duration % 60)
-else:
-    print('Вы ввели не число!')
